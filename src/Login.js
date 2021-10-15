36500ecacbce46efe8f2ecd44a3d5c9d2041c66c
Sadhana jayakumar <sadhanajayakumar@Sadhanas-MacBook-Pro.local>
2021-10-15 15:43:39 +0400

    Updated Login component to track email and password

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,14 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import "./Login.css"; 
 import { Link } from 'react-router-dom';
 
 function Login() {
+    const [email, setEmail] = useState(''); 
+    const [password, setPasswrod] = useState(''); 
+
+    const signIn = (event) =>{
+        event.preventDefault(); 
+    }
     return (
         <div className="login">
             <Link to="/">
@@ -12,12 +18,12 @@ function Login() {
                 <h1>Sign In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange = {e => setPasswrod(e.target.value)}/>
 
-                    <button className="login__signInButton">Sign In</button>
+                    <button type="submit" onClick={signIn} className="login__signInButton">Sign In</button>
                 </form>
                 <p>By signing-in you agree to Amazon's conditions of use and sale. Please see our privacy notice, our cookies notice and our interest based ads notice.</p>
                 <button className="login__registerButton">Create your Amazon Account</button>
